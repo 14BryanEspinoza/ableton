9d25339ce0ee440817306cd117f8dbb609aa58cb
Bryan Espinoza <14bryespinoza@gmail.com>
2026-08-27 14:29:32 -0600

    fix: add typescript eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
 import tailwind from "eslint-plugin-tailwindcss";
+import tseslint from "typescript-eslint";
 
 export default [
   {
@@ -8,6 +9,7 @@ export default [
   },
 
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   tailwind.configs.recommended,
 
