ccc3bef88e211b4904b5032d2659549ea56db39e
Bryan Espinoza <14bryespinoza@gmail.com>
2026-08-28 16:16:31 -0600

    feat: added footer components responsive

---
+++ src/data/links.ts
@@ -0,0 +1,42 @@
+interface LinksProps {
+  url: string;
+  label: string;
+}
+
+export const linkNavbar: LinksProps[] = [
+  { url: "#", label: "Home" },
+  { url: "#", label: "Projects" },
+  { url: "#", label: "About" },
+  { url: "#", label: "Services" },
+  { url: "#", label: "Blog" },
+  { url: "#", label: "Contact" },
+  { url: "#", label: "More +" },
+];
+
+export const linksHome: LinksProps[] = [
+  { url: "#", label: "Register For Live, Push or Move >" },
+  { url: "#", label: "About US >" },
+  { url: "#", label: "Jobs >" },
+];
+
+export const linksCommunity: LinksProps[] = [
+  { url: "#", label: "Find User Groups >" },
+  { url: "#", label: "Find Certified Training >" },
+  { url: "#", label: "Become a Certified Trainer >" },
+];
+
+export const linksEducation: LinksProps[] = [
+  { url: "#", label: "Offers for Students and Teachers >" },
+  { url: "#", label: "For the Classroom >" },
+  { url: "#", label: "For Colleges and Universities >" },
+];
+
+export const linkLegals: LinksProps[] = [
+  { url: "#", label: "Contact Us" },
+  { url: "#", label: "Press Resources" },
+  { url: "#", label: "Legal Information" },
+  { url: "#", label: "Corporate Information" },
+  { url: "#", label: "Privacy Policy" },
+  { url: "#", label: "Cookie Settings" },
+  { url: "#", label: "Print" },
+];
